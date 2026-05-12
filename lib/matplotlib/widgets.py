6ca47c12fd14522ebb0353dd22f1210fb5f0ffb4
Gabriel Silva <gabriel.da.silva@tecnico.ulisboa.pt>
2026-05-12 15:47:47 +0100

    complete_chain, place_vertex comments

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -4083,7 +4083,7 @@ class _PolygonalChainSelector(_SelectorWidget):
                 self._draw_polygonal_chain()
             self._active_handle_idx = -1
 
-        # Verify closed polygonal chain completion.
+        # Verify if button release is meant to complete the Closed polygonal chain.
         elif self._verify_completion(event):
             self._complete_chain(event)
 
@@ -4091,6 +4091,8 @@ class _PolygonalChainSelector(_SelectorWidget):
         elif (not self._selection_completed
               and 'move_all' not in self._state
               and 'move_vertex' not in self._state):
+            # IDEA: Remove the above branch
+            # and make that assessment in the _place_vertex method??????
             self._place_vertex(event)
 
         if self._selection_completed:
@@ -4103,7 +4105,7 @@ class _PolygonalChainSelector(_SelectorWidget):
         """Complete the polygonal chain."""
 
     def _place_vertex(self, event):
-        """Place a vertex at the position of the given event."""
+        """Place a vertex at the position of given event."""
 
     @_call_with_reparented_event
     def onmove(self, event):
@@ -4420,12 +4422,13 @@ class PolygonSelector(_PolygonalChainSelector):
                 and len(self._xys) > 3 and self._xys[-1] == self._xys[0])
 
     def _complete_chain(self, event):
+        """Complete the polygonal chain."""
         self._selection_completed = True
         if self._draw_box and self._box is None:
             self._add_box()
 
     def _place_vertex(self, event):
-        """Place a vertex at the position of the given event."""
+        """Place a vertex at the position of given event."""
         self._xys.insert(-1, (event.xdata, event.ydata))
 
     def _move_active_vertex(self, event):
@@ -4586,6 +4589,7 @@ class PolylineSelector(_PolygonalChainSelector):
                 and event.key == 'enter' and len(self._xys) > 1)
 
     def _complete_chain(self, event):
+        """Complete the polygonal chain."""
         self._xys.pop()
         self._selection_completed = True
         self._draw_polygonal_chain()
